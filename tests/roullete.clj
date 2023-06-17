557d985d14b153cee18171291653a419b6fc5ee8
Paulo Henrique <contato@paulohenriquesn.com>
2023-06-17 19:42:33 -0300

    test: separate two contexts tests

--- tests/roullete.clj
+++ tests/roullete.clj
@@ -1,17 +1,9 @@
 (ns tests.roullete
   (:require [clojure.test :refer :all]
-            [modules.roullete.roullete-service :as roullete-service]
-            [modules.user.user-service :as user-service]))
-(roullete-service/roll)
+            [modules.roullete.roullete-service :as roullete-service]))
 
 ;; Check if first bet is the same as second bet
 (is (not= (first (get (roullete-service/roll) :id))
           (first (get (roullete-service/roll) :id))))
 
 
-;; Check if first color is same as the second and throw if is
-(is (= true
-       (try
-         (do
-           (user-service/bet 1 ["green" "green"] 1) false)
-         (catch Exception e true))))
