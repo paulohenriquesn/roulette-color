797d065327dbca47fec56f937f747cdca1d02f41
Paulo Henrique <contato@paulohenriquesn.com>
2023-06-18 09:02:26 -0300

    feat: add user gets 100 coins when register

--- src/migrations/V20230617143009__user.sql
+++ src/migrations/V20230617143009__user.sql
@@ -2,6 +2,6 @@ CREATE TABLE "users" (
 id serial primary key,
 email text not null unique,
 password text not null,
-coin int default 0,
+coin int default 100,
 created_at timestamp without time zone default now()
 );
